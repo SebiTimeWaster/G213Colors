5724551d45f728b10bb44c78e94afce14d987f1a
TimeWaster <sebi@timewaster.de>
2017-10-02 20:01:02 +0200

    fixed bug with help from derEisele/G213Colors#4 , added user input validation, refactoring, bumped version number

--- G213Colors.py
+++ G213Colors.py
@@ -1,7 +1,7 @@
 '''
   *  The MIT License (MIT)
   *
-  *  G213Colors v0.1 Copyright (c) 2016 SebiTimeWaster
+  *  G213Colors v0.2 Copyright (c) 2016, 2017 SebiTimeWaster
   *
   *  Permission is hereby granted, free of charge, to any person obtaining a copy
   *  of this software and associated documentation files (the "Software"), to deal
@@ -27,26 +27,23 @@ import sys
 import usb.core
 import usb.util
 import binascii
-from time import sleep
 
 
-standardColor  = 'ffb4aa'         # Standard color, i found this color to produce a white color on my G213
-idVendor       = 0x046d           # The id of the Logitech company
-idProduct      = 0xc336           # The id of the G213
-bmRequestType  = 0x21             # --.
-bmRequest      = 0x09             #    \ The controll transfer
-wValue         = 0x0211           #    / configuration for the G213
-wIndex         = 0x0001           # --'
-colorCommand   = "11ff0c3a{}01{}0200000000000000000000"   # binary commands in hex format
-breatheCommand = "11ff0c3a0002{}{}006400000000000000"
-cycleCommand   = "11ff0c3a0003ffffff0000{}64000000000000"
-device         = ""               # device resource
-isDetached     = False            # If kernel driver needs to be reattached
-numArguments   = len(sys.argv)    # number of arguments given
-if numArguments > 1:
-    option     = str(sys.argv[1]) # option to use
-else:
-    option     = ""
+standardColorHex = 'ffb4aa'         # Standard color, i found this color to produce a white color on my G213
+idVendor         = 0x046d           # The id of the Logitech company
+idProduct        = 0xc336           # The id of the G213
+bEndpointAddress = 0x82             # Endpoint to read data back from
+bmRequestType    = 0x21             # --.
+bmRequest        = 0x09             #    \ The controll transfer
+wValue           = 0x0211           #    / configuration for the G213
+wIndex           = 0x0001           # --'
+colorCommand     = '11ff0c3a{}01{}0200000000000000000000'   # binary commands in hex format, always 20 byte long
+breatheCommand   = '11ff0c3a0002{}{}006400000000000000'
+cycleCommand     = '11ff0c3a0003ffffff0000{}64000000000000'
+device           = ''               # device resource
+isDetached       = False            # If kernel driver needs to be reattached
+numArguments     = len(sys.argv)    # number of arguments given
+option           = ''
 
 
 def connectG():
@@ -55,7 +52,8 @@ def connectG():
     device = usb.core.find(idVendor = idVendor, idProduct = idProduct)
     # if not found exit
     if device is None:
-        raise ValueError("USB device not found!")
+        print('USB device not found!')
+        sys.exit(1)
     # if a kernel driver is attached to the interface detach it, otherwise no data can be send
     if device.is_kernel_driver_active(wIndex):
         device.detach_kernel_driver(wIndex)
@@ -64,60 +62,101 @@ def connectG():
 def disconnectG():
     # free device resource to be able to reattach kernel driver
     usb.util.dispose_resources(device)
-    # reattach kernel driver, otherwise special key will not work
+    # reattach kernel driver, otherwise special keys will not work
     if isDetached:
         device.attach_kernel_driver(wIndex)
 
-def sendData(data):
-    # decode data to binary and send it
-    device.ctrl_transfer(bmRequestType, bmRequest, wValue, wIndex, binascii.unhexlify(data))
+def checkColorHex(colorHex):
+    try:
+        if len(colorHex) != 6:
+            raise ValueError()
+        int(colorHex, 16)
+    except:
+        pass
+        print('Not a valid hexadecimal color!')
+        return False
+    return True
+
+def checkSpeedNum(numStr):
+    try:
+        num = int(numStr)
+        if num < 32 or num > 65535:
+            raise ValueError()
+    except:
+        pass
+        print('Not a valid time in milliseconds!')
+        return False
+    return True
+
+def sendData(dataHex):
+    # convert hex data to binary and send it
+    device.ctrl_transfer(bmRequestType, bmRequest, wValue, wIndex, binascii.unhexlify(dataHex))
+    # read back one 20-byte word, otherwise commands may not be completely executed
+    device.read(bEndpointAddress, 20)
 
 def sendColorCommand(colorHex, field = 0):
-    sendData(colorCommand.format(str(format(field, '02x')), colorHex))
+    if checkColorHex(colorHex):
+        # convert number to hex
+        fieldHex = format(field, '02x')
+        commandHex = colorCommand.format(fieldHex, colorHex)
+        sendData(commandHex)
 
 def sendBreatheCommand(colorHex, speed):
-    sendData(breatheCommand.format(colorHex, str(format(speed, '04x'))))
+    if checkColorHex(colorHex) and checkSpeedNum(speed):
+        # convert number to hex
+        speedHex = format(int(speed), '04x')
+        commandHex = breatheCommand.format(colorHex, speedHex)
+        sendData(commandHex)
 
 def sendCycleCommand(speed):
-    sendData(cycleCommand.format(str(format(speed, '04x'))))
+    if checkSpeedNum(speed):
+        # convert number to hex
+        speedHex = format(int(speed), '04x')
+        commandHex = cycleCommand.format(speedHex)
+        sendData(commandHex)
 
 def printInfo():
-    print("G213Colors - Changes the key colors on a Logitech G213 Prodigy Gaming Keyboard")
-    print("\nOptions:")
-    print("-c                        Set the standard color (white)")
-    print("-c <color>                Set a custom color")
-    print("-c <color1> ... <color5>  Set custom colors for the 5 segments")
-    print("-b <color> <time>         Sets a color breathing animation")
-    print("-x <time>                 Sets a color cycling animation")
-    print("\nPlease note:")
-    print("* Color is a hex encoded color in the format RRGGBB")
-    print("  i.e. ff0000 is red, 00ff00 is green and so on,")
-    print("  abbreviated formats are not allowed")
-    print("* Time is in milliseconds, range: 32 - 65535")
+    print('G213Colors - Changes the key colors on a Logitech G213 Prodigy Gaming Keyboard')
+    print('')
+    print('Options:')
+    print('-c                        Set the standard color (white)')
+    print('-c <color>                Set a custom color')
+    print('-c <color1> ... <color5>  Set custom colors for all 5 segments')
+    print('-b <color> <time>         Sets a color breathing animation')
+    print('-x <time>                 Sets a color cycling animation')
+    print('')
+    print('Please note:')
+    print('* Color is a hex encoded color in the format RRGGBB')
+    print('  i.e. ff0000 is red, 00ff00 is green and so on,')
+    print('  abbreviated formats are not allowed')
+    print('* Time is in milliseconds in the range of 32 to 65535')
 
 
-if "-" not in option:
-    # no option found, exit
+# option to use
+if numArguments > 1:
+    option = str(sys.argv[1])
+# if no option found, exit
+if '-' not in option:
     printInfo()
     sys.exit(1)
 
 connectG()
 
-if "c" in option:
+# send command depending on option used and argument count
+if 'c' in option:
     if numArguments == 2:
-        sendColorCommand(standardColor)
+        sendColorCommand(standardColorHex)
     elif numArguments == 3:
-        sendColorCommand(str(sys.argv[2]))
+        sendColorCommand(sys.argv[2])
     elif numArguments == 7:
         for index in range(1, 6):
-            sendColorCommand(str(sys.argv[index + 1]), index)
-            sleep(0.01)
+            sendColorCommand(sys.argv[index + 1], index)
     else:
         printInfo()
-elif "b" in option and numArguments == 4:
-    sendBreatheCommand(str(sys.argv[2]), int(sys.argv[3]))
-elif "x" in option and numArguments == 3:
-    sendCycleCommand(int(sys.argv[2]))
+elif 'b' in option and numArguments == 4:
+    sendBreatheCommand(sys.argv[2], sys.argv[3])
+elif 'x' in option and numArguments == 3:
+    sendCycleCommand(sys.argv[2])
 else:
     printInfo()
 
--- README.md
+++ README.md
@@ -5,23 +5,23 @@ Please also check out these awesome forks:
 
 [G213Colors GUI](https://github.com/derEisele/G213Colors) - A cool grafical GUI you can use from the main menu
 
-[G213Colors GUI + installation script](https://github.com/JeroenED/G213Colors) - The GUI from derEisele plus an easy installation script
+[G213Colors GUI + installation script](https://github.com/JeroenED/G213Colors) - The GUI plus an easy installation script
 
 Thanks to [derEisele](https://github.com/derEisele) and [JeroenED](https://github.com/JeroenED) for creating this!
 
 ## What it does
 G213Colors lets you set the color(s) and certain effects of the illuminated keys on a G213 keyboard under Linux.
 
-Since Logitech is mostly ignoring the Linux market with their "Logitech Gaming Software" but i wanted to use my expensive new keyboard also under linux without tolerating the color cycling animation all the time i decided to reverse engineer their USB protocol and to write my own script to control the keyboard. 
+Since Logitech is mostly ignoring the Linux market with their "Logitech Gaming Software" but i wanted to use my expensive new keyboard also under linux without tolerating the color cycling animation all the time i decided to reverse engineer their USB protocol and to write my own script to control the keyboard.
 Also my keyboard is attached to an Aten KVM switch which interferes with the Logitech software to the degree that the computer becomes unusable in the worst case and the software does not start in the best case.
 
-G213Colors was built and tested under Linux for the G213 keyboard specifically, but after some adaptation it could potentially be run under other OS'es and used for other Logitech keyboards as well. 
+G213Colors was built and tested under Linux for the G213 keyboard specifically, but after some adaptation it could potentially be run under other OS'es and used for other Logitech keyboards as well.
 Please understand that i do not support any such adaptation, if you want to do it **you are on your own**.
 
 The "Wave" color effect that is available with the Logitech software could not be replicated since it is completely generated in the software by updating the colors every x ms (In contrast to the other effects which run on the keyboard itself). You could generate this effect with a script, but since G213Colors has to detach the kernel driver from one of the G213's interfaces to send data out the multimedia keys would most likely stop working. Unfortunately this is a side effect of the linux driver structure.
 
 ## Installation
-Either download the [G213Colors.py](https://raw.githubusercontent.com/SebiTimeWaster/G213Colors/master/G213Colors.py) file to a directory of your choosing **_or_** clone this project with git. 
+Either download the [G213Colors.py](https://raw.githubusercontent.com/SebiTimeWaster/G213Colors/master/G213Colors.py) file to a directory of your choosing **_or_** clone this project with git.
 
 ### Prerequisites
 * [Python](https://www.python.org/) >= 2.4 or 3.x (which is usually already installed)
@@ -38,9 +38,11 @@ sudo python G213Colors.py
 
 G213Colors needs to be run as root as long as your user doesn't have access privileges for that USB device ([How to do this](http://stackoverflow.com/a/32022908/2948666), please use "046d" as idVendor and "c336" as idProduct).
 
-### Known issues
-* Setting the color for all 5 "fields" separately works shaky at best, usually some fields are skipped. If color changes don't work anymore try pulling the keyboard and plug it in again.
-
 ## Changelog
+Changelog v0.2:
+* Fixed bug preventing setting all 5 color fields correctly, thanks to @oncecreated for finding this solution!
+* Added propper validation of user inputs
+* Complete refactoring
+
 Changelog v0.1:
 * Initial checkin
